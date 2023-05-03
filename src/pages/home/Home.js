15b99004fca2bd955a3e06fc7593401e6da82064
HafizaSheeza <118974851+HafizaSheeza@users.noreply.github.com>
2023-05-03 23:20:53 +0500

    first commit

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import Slider from '../../component/slider/Slider'
+
 import FlashSale from '../../component/home/flashsale/FlashSale'
 import Category from '../../component/category/Category'
 import JustFor from '../../component/home/justforyou/JustFor'
 
 function Home() {
   return (
- < div style={{
-  background: '#eff0f5',
- }}>
- <Slider/>
- <FlashSale/>
- <Category/>
- <JustFor/>
- </div>
+    < div style={{
+      background: '#eff0f5',
+    }}>
+
+      <FlashSale />
+      <Category />
+      <JustFor />
+    </div>
   )
 }
 
